c1a277fc43c603028205cc44f283c18492827e39
Kishon Daniels <kishondaniels@gmail.com>
2015-06-09 18:24:42 -0500

    Update Home.m

--- Home.m
+++ Home.m
@@ -16,9 +16,12 @@
     
     [[_loginButton layer] setBorderWidth:1.0f];
     [[_loginButton layer] setBorderColor:[UIColor whiteColor].CGColor];
+
     _signUpButton.layer.cornerRadius = 7; // this value varies
     _signUpButton.clipsToBounds = YES;
+
     _loginButton.layer.cornerRadius = 7; // this value vary as per your desire
     _loginButton.clipsToBounds = YES;
+
 }
 @end
