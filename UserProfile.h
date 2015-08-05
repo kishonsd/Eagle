ec592eb7128ad58b13c44f5a3be5f681230c003a
Kishon Daniels <kishondaniels@gmail.com>
2015-08-05 09:56:44 -0500

    Update UserProfile.h

--- UserProfile.h
+++ UserProfile.h
@@ -1,1 +1,20 @@
 
+//
+//  PFQueryTableViewController+UserProfile.h
+//  Bash
+//
+//  Created by Kishon Daniels on 7/28/15.
+//  Copyright (c) 2015 HackStack. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+#import <Parse/Parse.h>
+
+@interface UserProfile : PFQueryTableViewController <UITableViewDataSource, UITableViewDataSource> {
+    
+    NSArray *posts;
+}
+
+@property (nonatomic, strong) IBOutlet UINavigationBar *navBar;
+
+@end
