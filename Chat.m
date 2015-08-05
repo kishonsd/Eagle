204c9d5f97e91549ee786c84b498a01f95cc3a4e
Kishon Daniels <kishondaniels@gmail.com>
2015-08-05 09:39:37 -0500

    Update Chat.m

--- Chat.m
+++ Chat.m
@@ -1,1 +1,365 @@
 
+//
+//  PFQueryTableViewController+Chat.m
+//  Bash
+//
+//  Created by Kishon Daniels on 7/30/15.
+//  Copyright (c) 2015 HackStack. All rights reserved.
+//
+
+#import "Chat.h"
+
+@implementation Chat
+
+
+{
+    CGFloat startContentOffset;
+    CGFloat lastContentOffset;
+    BOOL hidden;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        
+        // The className to query on
+        self.parseClassName = @"Chat";
+        
+        // The key of the PFObject to display in the label of the default cell style
+        self.textKey = @"text";
+        
+        // The title for this table in the Navigation Controller.
+        self.title = @"Chat";
+        
+        // Whether the built-in pull-to-refresh is enabled
+        self.pullToRefreshEnabled = YES;
+        
+        // Whether the built-in pagination is enabled
+        self.paginationEnabled = NO;
+        
+        // The number of objects to show per page
+        self.objectsPerPage = 100;
+    }
+    
+    return self;
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    _navBar.topItem.title = @"Chat";
+    
+    [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys: [UIFont fontWithName:@"Avenir - Medium" size:21], NSFontAttributeName, nil]];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.navigationController setNavigationBarHidden:hidden
+                                             animated:NO];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return posts.count;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - Parse
+
+- (void)objectsDidLoad:(NSError *)error {
+    [super objectsDidLoad:error];
+    
+    // This method is called every time objects are loaded from Parse via the PFQuery
+}
+
+- (void)objectsWillLoad {
+    [super objectsWillLoad];
+    
+    // This method is called before a PFQuery is fired to get more objects
+}
+
+
+// Override to customize what kind of query to perform on the class. The default is to query for
+// all objects ordered by createdAt descending.
+- (PFQuery *)queryForTable {
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"Chat"];
+    
+    // If no objects are loaded in memory, we look to the cache first to fill the table
+    // and then subsequently do a query against the network.
+    if ([self.objects count] == 0) {
+        query.cachePolicy = kPFCachePolicyCacheThenNetwork;
+    }
+    
+    [query orderByAscending:@"createdAt"];
+    
+    return query;
+}
+
+#pragma mark - The Magic!
+
+- (void)expand
+{
+    if(hidden)
+        return;
+    
+    hidden = YES;
+    
+    [self.navigationController setNavigationBarHidden:YES
+                                             animated:YES];
+}
+
+- (void)contract
+{
+    if(!hidden)
+        return;
+    
+    hidden = NO;
+    
+    [self.navigationController setNavigationBarHidden:NO
+                                             animated:YES];
+}
+
+#pragma mark -
+#pragma mark UIScrollViewDelegate Methods
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    startContentOffset = lastContentOffset = scrollView.contentOffset.y;
+    //NSLog(@"scrollViewWillBeginDragging: %f", scrollView.contentOffset.y);
+}
+
+- (void)scrollViewScroll:(UIScrollView *)scrollView
+{
+    CGFloat currentOffset = scrollView.contentOffset.y;
+    CGFloat differenceFromStart = startContentOffset - currentOffset;
+    CGFloat differenceFromLast = lastContentOffset - currentOffset;
+    lastContentOffset = currentOffset;
+    
+    
+    
+    if((differenceFromStart) < 0)
+    {
+        // scroll up
+        if(scrollView.isTracking && (fabs(differenceFromLast)>1))
+            [self expand];
+    }
+    else {
+        
+        if(scrollView.isTracking && (fabs(differenceFromLast)>1))
+            [self contract];
+    }
+    
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+}
+
+- (BOOL)scrollViewShouldScrollToTop:(UIScrollView *)scrollView
+{
+    [self contract];
+    return YES;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    // Get the current size of the refresh controller
+    CGRect refreshBounds = self.refreshControl.bounds;
+    
+    // Distance the table has been pulled >= 0
+    CGFloat pullDistance = MAX(0.0, -self.refreshControl.frame.origin.y);
+    
+    // Half the width of the table
+    CGFloat midX = self.tableView.frame.size.width / 2.0;
+    
+    // Calculate the width and height of our graphics
+    CGFloat compassHeight = self.compass_background.bounds.size.height;
+    CGFloat compassHeightHalf = compassHeight / 2.0;
+    
+    CGFloat compassWidth = self.compass_background.bounds.size.width;
+    CGFloat compassWidthHalf = compassWidth / 2.0;
+    
+    CGFloat spinnerHeight = self.compass_spinner.bounds.size.height;
+    CGFloat spinnerHeightHalf = spinnerHeight / 2.0;
+    
+    CGFloat spinnerWidth = self.compass_spinner.bounds.size.width;
+    CGFloat spinnerWidthHalf = spinnerWidth / 2.0;
+    
+    // Calculate the pull ratio, between 0.0-1.0
+    CGFloat pullRatio = MIN( MAX(pullDistance, 0.0), 100.0) / 100.0;
+    
+    // Set the Y coord of the graphics, based on pull distance
+    CGFloat compassY = pullDistance / 2.0 - compassHeightHalf;
+    CGFloat spinnerY = pullDistance / 2.0 - spinnerHeightHalf;
+    
+    // Calculate the X coord of the graphics, adjust based on pull ratio
+    CGFloat compassX = (midX + compassWidthHalf) - (compassWidth * pullRatio);
+    CGFloat spinnerX = (midX - spinnerWidth - spinnerWidthHalf) + (spinnerWidth * pullRatio);
+    
+    // When the compass and spinner overlap, keep them together
+    if (fabs(compassX - spinnerX) < 1.0) {
+        self.isRefreshIconsOverlap = YES;
+    }
+    
+    // If the graphics have overlapped or we are refreshing, keep them together
+    if (self.isRefreshIconsOverlap || self.refreshControl.isRefreshing) {
+        compassX = midX - compassWidthHalf;
+        spinnerX = midX - spinnerWidthHalf;
+    }
+    
+    // Set the graphic's frames
+    CGRect compassFrame = self.compass_background.frame;
+    compassFrame.origin.x = compassX;
+    compassFrame.origin.y = compassY;
+    
+    CGRect spinnerFrame = self.compass_spinner.frame;
+    spinnerFrame.origin.x = spinnerX;
+    spinnerFrame.origin.y = spinnerY;
+    
+    self.compass_background.frame = compassFrame;
+    self.compass_spinner.frame = spinnerFrame;
+    
+    // Set the encompassing view's frames
+    refreshBounds.size.height = pullDistance;
+    
+    self.refreshColorView.frame = refreshBounds;
+    self.refreshLoadingView.frame = refreshBounds;
+    
+    // If we're refreshing and the animation is not playing, then play the animation
+    if (self.refreshControl.isRefreshing && !self.isRefreshAnimating) {
+        [self animateRefreshView];
+    }
+    
+    NSLog(@"pullDistance: %.1f, pullRatio: %.1f, midX: %.1f, isRefreshing: %i", pullDistance, pullRatio, midX, self.refreshControl.isRefreshing);
+}
+
+- (void)animateRefreshView
+{
+    // Background color to loop through for our color view
+    NSArray *colorArray = @[[UIColor redColor],[UIColor blueColor],[UIColor purpleColor],[UIColor cyanColor],[UIColor orangeColor],[UIColor magentaColor]];
+    static int colorIndex = 0;
+    
+    // Flag that we are animating
+    self.isRefreshAnimating = YES;
+    
+    [UIView animateWithDuration:0.3
+                          delay:0
+                        options:UIViewAnimationOptionCurveLinear
+                     animations:^{
+                         // Rotate the spinner by M_PI_2 = PI/2 = 90 degrees
+                         [self.compass_spinner setTransform:CGAffineTransformRotate(self.compass_spinner.transform, M_PI_2)];
+                         
+                         // Change the background color
+                         self.refreshColorView.backgroundColor = [colorArray objectAtIndex:colorIndex];
+                         colorIndex = (colorIndex + 1) % colorArray.count;
+                     }
+                     completion:^(BOOL finished) {
+                         // If still refreshing, keep spinning, else reset
+                         if (self.refreshControl.isRefreshing) {
+                             [self animateRefreshView];
+                         }else{
+                             [self resetAnimation];
+                         }
+                     }];
+}
+
+- (void)resetAnimation
+{
+    // Reset our flags and background color
+    self.isRefreshAnimating = NO;
+    self.isRefreshIconsOverlap = NO;
+    self.refreshColorView.backgroundColor = [UIColor clearColor];
+}
+
+
+// Override to customize the look of a cell representing an object. The default is to display
+// a UITableViewCellStyleDefault style cell with the label being the first key in the object.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    UILabel *eventName = (UILabel*) [cell viewWithTag:1];
+    eventName.text = [object objectForKey:@"text"];
+    
+    
+    //loading icons
+    PFFile *thumbnail = [object objectForKey:@"eventImage"];
+    PFImageView *thumbnailImageView = (PFImageView*)[cell viewWithTag:4];
+    thumbnailImageView.file = thumbnail;
+    thumbnailImageView.image = [UIImage imageNamed:@"placeholder.jpg"];
+    
+    
+    [thumbnailImageView loadInBackground:^(UIImage *image, NSError *error) {
+        if (!error) {
+            
+        }
+    }];
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [super tableView:tableView didSelectRowAtIndexPath:indexPath];
+}
