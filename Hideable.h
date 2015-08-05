c018a281d0d39b33ed350092d444aa7ebf11ef74
Kishon Daniels <kishondaniels@gmail.com>
2015-08-05 10:00:35 -0500

    Update Hideable.h

--- Hideable.h
+++ Hideable.h
@@ -1,1 +1,16 @@
-H
+
+//
+//  UITableViewController+Hideable.h
+//  Bash
+//
+//  Created by Kishon Daniels on 8/4/15.
+//  Copyright (c) 2015 HackStack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Hideable : UITabBarController
+
+- (void)setTabBarHidden:(BOOL)hidden animated:(BOOL)animated;
+
+@end
